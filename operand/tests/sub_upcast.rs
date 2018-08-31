6e4d5b7f2b6f560056e16f2854b1356eee04a24c
cledant <cledant@student.42.fr>
2018-08-31 15:28:45 +0200

    added sub operator + tests

---
+++ operand/tests/sub_upcast.rs
@@ -0,0 +1,107 @@
+extern crate operand;
+
+use operand::value::Type;
+
+//From Char
+#[test]
+fn operand_sub_upcast_ok_1() {
+	assert_eq!(Ok(Type::Short(25)), Type::Short(50) - Type::Char(25));
+}
+
+#[test]
+fn operand_sub_upcast_ok_2() {
+	assert_eq!(Ok(Type::Int(25)), Type::Int(50) - Type::Char(25));
+}
+
+#[test]
+fn operand_sub_upcast_ok_3() {
+	assert_eq!(Ok(Type::Float(25.00)), Type::Float(50.00) - Type::Char(25));
+}
+
+#[test]
+fn operand_sub_upcast_ok_4() {
+	assert_eq!(Ok(Type::Double(25.00)), Type::Double(50.00) - Type::Char(25));
+}
+
+#[test]
+fn operand_sub_upcast_ok_5() {
+	assert_eq!(Ok(Type::Short(-25)), Type::Char(25) - Type::Short(50));
+}
+
+#[test]
+fn operand_sub_upcast_ok_6() {
+	assert_eq!(Ok(Type::Int(-25)), Type::Char(25) - Type::Int(50));
+}
+
+#[test]
+fn operand_sub_upcast_ok_7() {
+	assert_eq!(Ok(Type::Float(-25.00)), Type::Char(25) -Type::Float(50.00));
+}
+
+#[test]
+fn operand_sub_upcast_ok_8() {
+	assert_eq!(Ok(Type::Double(-25.00)), Type::Char(25) -Type::Double(50.00));
+}
+
+//From Short
+#[test]
+fn operand_sub_upcast_ok_9() {
+	assert_eq!(Ok(Type::Int(25)), Type::Int(50) - Type::Short(25));
+}
+
+#[test]
+fn operand_sub_upcast_ok_10() {
+	assert_eq!(Ok(Type::Float(25.00)), Type::Float(50.00) - Type::Short(25));
+}
+
+#[test]
+fn operand_sub_upcast_ok_11() {
+	assert_eq!(Ok(Type::Double(25.00)), Type::Double(50.00) - Type::Short(25));
+}
+
+#[test]
+fn operand_sub_upcast_ok_12() {
+	assert_eq!(Ok(Type::Int(-25)), Type::Short(25) - Type::Int(50));
+}
+
+#[test]
+fn operand_sub_upcast_ok_13() {
+	assert_eq!(Ok(Type::Float(-25.00)), Type::Short(25) - Type::Float(50.00));
+}
+
+#[test]
+fn operand_sub_upcast_ok_14() {
+	assert_eq!(Ok(Type::Double(-25.00)), Type::Short(25) - Type::Double(50.00));
+}
+
+//From Int
+#[test]
+fn operand_sub_upcast_ok_15() {
+	assert_eq!(Ok(Type::Float(25.00)), Type::Int(50) - Type::Float(25.00));
+}
+
+#[test]
+fn operand_sub_upcast_ok_16() {
+	assert_eq!(Ok(Type::Double(25.00)), Type::Int(50) - Type::Double(25.00));
+}
+
+#[test]
+fn operand_sub_upcast_ok_17() {
+	assert_eq!(Ok(Type::Float(-25.00)), Type::Float(25.00) - Type::Int(50));
+}
+
+#[test]
+fn operand_sub_upcast_ok_18() {
+	assert_eq!(Ok(Type::Double(-25.00)), Type::Double(25.00) - Type::Int(50));
+}
+
+//From Float
+#[test]
+fn operand_sub_upcast_ok_19() {
+	assert_eq!(Ok(Type::Double(25.00)), Type::Float(50.00) - Type::Double(25.00));
+}
+
+#[test]
+fn operand_sub_upcast_ok_20() {
+	assert_eq!(Ok(Type::Double(-25.00)), Type::Double(25.00) - Type::Float(50.00));
+}
