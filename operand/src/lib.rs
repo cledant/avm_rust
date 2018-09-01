7ffed6bc0c59b7bfdfd111a4a7a71138bc6caf24
cledant <cledant@student.42.fr>
2018-09-01 12:49:53 +0200

    added div operand + tests

--- operand/src/lib.rs
+++ operand/src/lib.rs
@@ -5,6 +5,7 @@ extern crate num;
 use std::ops::Add;
 use std::ops::Sub;
 use std::ops::Mul;
+use std::ops::Div;
 use std::num::FpCategory;
 
 	#[derive(Debug, PartialEq)]
@@ -26,6 +27,8 @@ use std::num::FpCategory;
 
 	pub static ERR_OVERFLOW : &str = "Overflow detected";
 	pub static ERR_IMPOSSIBLE : &str = "Impossible case";
+	pub static ERR_DIV_0 : &str = "Divided by 0 or too small number";
+	pub static ERR_NAN : &str = "Float result is not a number";
 
 	//Add
 	#[inline]
@@ -36,7 +39,7 @@ use std::num::FpCategory;
 		match (val.classify(), ret) {
 			(FpCategory::Subnormal, _) => Err(ERR_OVERFLOW),
 			(FpCategory::Infinite, _) => Err(ERR_OVERFLOW),
-			(FpCategory::Nan, _) => Err(ERR_OVERFLOW),
+			(FpCategory::Nan, _) => Err(ERR_NAN),
 			(_, RetType::Float) => Ok(Type::Float(val.to_f32().unwrap())),
 			(_, RetType::Double) => Ok(Type::Double(val.to_f64().unwrap())),
 			(_, _) => Err(ERR_IMPOSSIBLE),
@@ -104,7 +107,7 @@ use std::num::FpCategory;
 		match (val.classify(), ret) {
 			(FpCategory::Subnormal, _) => Err(ERR_OVERFLOW),
 			(FpCategory::Infinite, _) => Err(ERR_OVERFLOW),
-			(FpCategory::Nan, _) => Err(ERR_OVERFLOW),
+			(FpCategory::Nan, _) => Err(ERR_NAN),
 			(_, RetType::Float) => Ok(Type::Float(val.to_f32().unwrap())),
 			(_, RetType::Double) => Ok(Type::Double(val.to_f64().unwrap())),
 			(_, _) => Err(ERR_IMPOSSIBLE),
@@ -163,7 +166,7 @@ use std::num::FpCategory;
 		}
 	}
 
-	//Mult Operator
+	//Mul Operator
 	#[inline]
 	fn checked_mul_float<T> (x : T, y : T, ret : RetType) -> Result<Type, &'static str> 
 	where T : num::Float + num::cast::ToPrimitive
@@ -172,7 +175,7 @@ use std::num::FpCategory;
 		match (val.classify(), ret) {
 			(FpCategory::Subnormal, _) => Err(ERR_OVERFLOW),
 			(FpCategory::Infinite, _) => Err(ERR_OVERFLOW),
-			(FpCategory::Nan, _) => Err(ERR_OVERFLOW),
+			(FpCategory::Nan, _) => Err(ERR_NAN),
 			(_, RetType::Float) => Ok(Type::Float(val.to_f32().unwrap())),
 			(_, RetType::Double) => Ok(Type::Double(val.to_f64().unwrap())),
 			(_, _) => Err(ERR_IMPOSSIBLE),
@@ -227,6 +230,74 @@ use std::num::FpCategory;
 				(Type::Int(x), Type::Double(y)) => checked_mul_float(x as f64, y, RetType::Double),
 				(Type::Float(x), Type::Double(y)) => checked_mul_float(x as f64, y, RetType::Double),
 				(Type::Double(x), Type::Double(y)) => checked_mul_float(x, y, RetType::Double),
+			}
+		}
+	}
+
+	//Div Operator
+	#[inline]
+	fn checked_div_float<T> (x : T, y : T, ret : RetType) -> Result<Type, &'static str> 
+	where T : num::Float + num::cast::ToPrimitive
+	{
+		let val = x / y;
+		match (val.classify(), ret) {
+			(FpCategory::Subnormal, _) => Err(ERR_DIV_0),
+			(FpCategory::Infinite, _) => Err(ERR_DIV_0),
+			(FpCategory::Nan, _) => Err(ERR_NAN),
+			(_, RetType::Float) => Ok(Type::Float(val.to_f32().unwrap())),
+			(_, RetType::Double) => Ok(Type::Double(val.to_f64().unwrap())),
+			(_, _) => Err(ERR_IMPOSSIBLE),
+		}
+	}
+
+	#[inline]
+	fn checked_div_integer<T> (x : T, y : T, ret : RetType) -> Result<Type, &'static str> 
+	where T : num::CheckedDiv + num::cast::ToPrimitive
+	{
+		match (x.checked_div(&y), ret) {
+			(Some(nb) , RetType::Char) => Ok(Type::Char(nb.to_i8().unwrap())),
+			(Some(nb) , RetType::Short) => Ok(Type::Short(nb.to_i16().unwrap())),
+			(Some(nb) , RetType::Int) => Ok(Type::Int(nb.to_i32().unwrap())),
+			(Some(_) , _) => Err(ERR_IMPOSSIBLE),
+			(None, _) => Err(ERR_DIV_0),
+		}	
+	}
+
+	impl Div for Type {
+		type Output = Result<Type, &'static str>;
+
+		fn div(self, other : Type) -> Result<Type, &'static str> {
+			match (self, other) {
+				//Char
+				(Type::Char(x), Type::Char(y)) => checked_div_integer(x, y, RetType::Char),
+				(Type::Short(x), Type::Char(y)) => checked_div_integer(x, y as i16, RetType::Short),
+				(Type::Int(x), Type::Char(y)) => checked_div_integer(x, y as i32, RetType::Int),
+				(Type::Float(x), Type::Char(y)) => checked_div_float(x, y as f32, RetType::Float),
+				(Type::Double(x), Type::Char(y)) => checked_div_float(x, y as f64, RetType::Double),
+				//Short
+				(Type::Char(x), Type::Short(y)) => checked_div_integer(x as i16, y, RetType::Short),
+				(Type::Short(x), Type::Short(y)) => checked_div_integer(x, y, RetType::Short),
+				(Type::Int(x), Type::Short(y)) => checked_div_integer(x, y as i32, RetType::Int),
+				(Type::Float(x), Type::Short(y)) => checked_div_float(x, y as f32, RetType::Float),
+				(Type::Double(x), Type::Short(y)) => checked_div_float(x, y as f64, RetType::Double),
+				//Int
+				(Type::Char(x), Type::Int(y)) => checked_div_integer(x as i32, y, RetType::Int),
+				(Type::Short(x), Type::Int(y)) => checked_div_integer(x as i32, y, RetType::Int),
+				(Type::Int(x), Type::Int(y)) => checked_div_integer(x, y, RetType::Int),
+				(Type::Float(x), Type::Int(y)) => checked_div_float(x, y as f32, RetType::Float),
+				(Type::Double(x), Type::Int(y)) => checked_div_float(x, y as f64, RetType::Double),
+				//Float
+				(Type::Char(x), Type::Float(y)) => checked_div_float(x as f32, y, RetType::Float),
+				(Type::Short(x), Type::Float(y)) => checked_div_float(x as f32, y, RetType::Float),
+				(Type::Int(x), Type::Float(y)) => checked_div_float(x as f32, y, RetType::Float),
+				(Type::Float(x), Type::Float(y)) => checked_div_float(x, y, RetType::Float),
+				(Type::Double(x), Type::Float(y)) => checked_div_float(x, y as f64, RetType::Double),
+				//Double
+				(Type::Char(x), Type::Double(y)) => checked_div_float(x as f64, y, RetType::Double),
+				(Type::Short(x), Type::Double(y)) => checked_div_float(x as f64, y, RetType::Double),
+				(Type::Int(x), Type::Double(y)) => checked_div_float(x as f64, y, RetType::Double),
+				(Type::Float(x), Type::Double(y)) => checked_div_float(x as f64, y, RetType::Double),
+				(Type::Double(x), Type::Double(y)) => checked_div_float(x, y, RetType::Double),
 			}
 		}
 	}
---
+++ operand/tests/div_basic.rs
@@ -0,0 +1,68 @@
+extern crate operand;
+
+use operand::value::Type;
+
+//Char
+#[test]
+fn operand_div_char_ok() {
+	assert_eq!(Ok(Type::Char(25)), Type::Char(50) / Type::Char(2));
+}
+
+#[test]
+fn operand_div_0_char_failed() {
+	assert_eq!(Err(operand::value::ERR_DIV_0), Type::Char(50) / Type::Char(0));
+}
+
+//Short
+#[test]
+fn operand_div_short_ok() {
+	assert_eq!(Ok(Type::Short(16000)), Type::Short(32000) / Type::Short(2));
+}
+
+#[test]
+fn operand_div_0_short_failed() {
+	assert_eq!(Err(operand::value::ERR_DIV_0), Type::Short(10000) / Type::Short(0));
+}
+
+//Int
+#[test]
+fn operand_div_int_ok() {
+	assert_eq!(Ok(Type::Int(0)), Type::Int(50) / Type::Int(500000));
+}
+
+#[test]
+fn operand_div_0_int_failed() {
+	assert_eq!(Err(operand::value::ERR_DIV_0), Type::Int(2000000000) / Type::Int(0));
+}
+
+//Float
+#[test]
+fn operand_div_float_ok() {
+	assert_eq!(Ok(Type::Float(10000.5)), Type::Float(20001.0) / Type::Float(2.0));
+}
+
+#[test]
+fn operand_div_0_float_failed() {
+	assert_eq!(Err(operand::value::ERR_DIV_0), Type::Float(std::f32::MAX) / Type::Float(0.0));
+}
+
+#[test]
+fn operand_div_overflow_float_failed() {
+	assert_eq!(Err(operand::value::ERR_DIV_0), Type::Float(1.0e+20_f32) / Type::Float(1.0e-20_f32));
+}
+
+//Double
+#[test]
+fn operand_div_double_ok() {
+	assert_eq!(Ok(Type::Double(10000.5)), Type::Double(20001.0) / Type::Double(2.0));
+}
+
+#[test]
+fn operand_div_0_double_failed() {
+	assert_eq!(Err(operand::value::ERR_DIV_0), Type::Double(10000.0) / Type::Double(0.0));
+}
+
+#[test]
+fn operand_div_overflow_double_failed() {
+	assert_eq!(Err(operand::value::ERR_DIV_0), Type::Double(1.0e+300_f64) / Type::Double(1.0e-300_f64));
+}
