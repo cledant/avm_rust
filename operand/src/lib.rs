a4f7f5eb5c0186f1462b26b64f38308061ccad66
cledant <cledant@student.42.fr>
2018-09-01 15:41:43 +0200

    added rem operand + tests

--- operand/src/lib.rs
+++ operand/src/lib.rs
@@ -1,11 +1,13 @@
 pub mod value {
 
 extern crate num;
+extern crate num_traits;
 
 use std::ops::Add;
 use std::ops::Sub;
 use std::ops::Mul;
 use std::ops::Div;
+use std::ops::Rem;
 use std::num::FpCategory;
 
 	#[derive(Debug, PartialEq)]
@@ -298,6 +300,74 @@ use std::num::FpCategory;
 				(Type::Int(x), Type::Double(y)) => checked_div_float(x as f64, y, RetType::Double),
 				(Type::Float(x), Type::Double(y)) => checked_div_float(x as f64, y, RetType::Double),
 				(Type::Double(x), Type::Double(y)) => checked_div_float(x, y, RetType::Double),
+			}
+		}
+	}
+
+	//Mod Operator
+	#[inline]
+	fn checked_rem_float<T> (x : T, y : T, ret : RetType) -> Result<Type, &'static str> 
+	where T : num::Float + num::cast::ToPrimitive
+	{
+		let val = x % y;
+		match (val.classify(), ret) {
+			(FpCategory::Subnormal, _) => Err(ERR_DIV_0),
+			(FpCategory::Infinite, _) => Err(ERR_DIV_0),
+			(FpCategory::Nan, _) => Err(ERR_NAN),
+			(_, RetType::Float) => Ok(Type::Float(val.to_f32().unwrap())),
+			(_, RetType::Double) => Ok(Type::Double(val.to_f64().unwrap())),
+			(_, _) => Err(ERR_IMPOSSIBLE),
+		}
+	}
+
+	#[inline]
+	fn checked_rem_integer<T> (x : T, y : T, ret : RetType) -> Result<Type, &'static str> 
+	where T : num_traits::CheckedRem + num::cast::ToPrimitive
+	{
+		match (x.checked_rem(&y), ret) {
+			(Some(nb) , RetType::Char) => Ok(Type::Char(nb.to_i8().unwrap())),
+			(Some(nb) , RetType::Short) => Ok(Type::Short(nb.to_i16().unwrap())),
+			(Some(nb) , RetType::Int) => Ok(Type::Int(nb.to_i32().unwrap())),
+			(Some(_) , _) => Err(ERR_IMPOSSIBLE),
+			(None, _) => Err(ERR_DIV_0),
+		}	
+	}
+
+	impl Rem for Type {
+		type Output = Result<Type, &'static str>;
+
+		fn rem(self, other : Type) -> Result<Type, &'static str> {
+			match (self, other) {
+				//Char
+				(Type::Char(x), Type::Char(y)) => checked_rem_integer(x, y, RetType::Char),
+				(Type::Short(x), Type::Char(y)) => checked_rem_integer(x, y as i16, RetType::Short),
+				(Type::Int(x), Type::Char(y)) => checked_rem_integer(x, y as i32, RetType::Int),
+				(Type::Float(x), Type::Char(y)) => checked_rem_float(x, y as f32, RetType::Float),
+				(Type::Double(x), Type::Char(y)) => checked_rem_float(x, y as f64, RetType::Double),
+				//Short
+				(Type::Char(x), Type::Short(y)) => checked_rem_integer(x as i16, y, RetType::Short),
+				(Type::Short(x), Type::Short(y)) => checked_rem_integer(x, y, RetType::Short),
+				(Type::Int(x), Type::Short(y)) => checked_rem_integer(x, y as i32, RetType::Int),
+				(Type::Float(x), Type::Short(y)) => checked_rem_float(x, y as f32, RetType::Float),
+				(Type::Double(x), Type::Short(y)) => checked_rem_float(x, y as f64, RetType::Double),
+				//Int
+				(Type::Char(x), Type::Int(y)) => checked_rem_integer(x as i32, y, RetType::Int),
+				(Type::Short(x), Type::Int(y)) => checked_rem_integer(x as i32, y, RetType::Int),
+				(Type::Int(x), Type::Int(y)) => checked_rem_integer(x, y, RetType::Int),
+				(Type::Float(x), Type::Int(y)) => checked_rem_float(x, y as f32, RetType::Float),
+				(Type::Double(x), Type::Int(y)) => checked_rem_float(x, y as f64, RetType::Double),
+				//Float
+				(Type::Char(x), Type::Float(y)) => checked_rem_float(x as f32, y, RetType::Float),
+				(Type::Short(x), Type::Float(y)) => checked_rem_float(x as f32, y, RetType::Float),
+				(Type::Int(x), Type::Float(y)) => checked_rem_float(x as f32, y, RetType::Float),
+				(Type::Float(x), Type::Float(y)) => checked_rem_float(x, y, RetType::Float),
+				(Type::Double(x), Type::Float(y)) => checked_rem_float(x, y as f64, RetType::Double),
+				//Double
+				(Type::Char(x), Type::Double(y)) => checked_rem_float(x as f64, y, RetType::Double),
+				(Type::Short(x), Type::Double(y)) => checked_rem_float(x as f64, y, RetType::Double),
+				(Type::Int(x), Type::Double(y)) => checked_rem_float(x as f64, y, RetType::Double),
+				(Type::Float(x), Type::Double(y)) => checked_rem_float(x as f64, y, RetType::Double),
+				(Type::Double(x), Type::Double(y)) => checked_rem_float(x, y, RetType::Double),
 			}
 		}
 	}
