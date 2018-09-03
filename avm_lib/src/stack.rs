680dd6274a9849b9437cc87e2eb4d9a9d8caa451
cledant <cledant@student.42.fr>
2018-09-03 13:56:16 +0200

    added run method for stack + WIP stack test

--- avm_lib/src/stack.rs
+++ avm_lib/src/stack.rs
@@ -1,5 +1,39 @@
+use parser;
+use value;
+
+pub static ERR_INVALID_TOKEN : &str = "Invalid Token";
+pub static ERR_NO_EXIT_INST : &str = "No Exit Instruction";
+
 pub enum ExecState {
 	Continue,
 	Stop,
 	Error(&'static str),
+}
+
+pub struct Stack {
+	vec_value : Vec<value::Type>,
+}
+
+impl Stack {
+	pub fn run(&mut self, entry : &mut Vec<parser::Token>) {
+		while let Some(token) = entry.pop() {
+			match token.inst {
+				Some(fct) => {
+					match fct(token.val, &mut self.vec_value) {
+						ExecState::Error(e) => {
+							println!("Error : {:?}", e);
+							return;
+						}
+						ExecState::Stop => { return; }
+						ExecState::Continue => {}
+					}
+				}
+				None => { 
+					println!("Error : {:?}", ERR_INVALID_TOKEN);
+					return;
+				}
+			};
+		}
+		println!("Error : {:?}", ERR_NO_EXIT_INST);	
+	}
 }
---
+++ avm_lib/tests/stack_basic.rs
@@ -0,0 +1,10 @@
+extern crate avm_lib;
+
+use avm_lib::stack;
+use avm_lib::parser::Token;
+use avm_lib::value::Type;
+
+#[test]
+/*fn stack_push() {
+	let tok = Token(Some(Type::Char(42), )); 
+}*/
